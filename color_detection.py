2e19016b44523fabe962e757996528f9312f6163
Phi Tran <phitrann0810@gmail.com>
2025-05-10 17:34:54 -0500

    circle

--- color_detection.py
+++ color_detection.py
@@ -50,27 +50,18 @@ cv2.namedWindow('image')
 cv2.setMouseCallback('image',draw_function)
 
 while(1):
-
-    cv2.imshow("image",img)
-    if (clicked):
-   
-        #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
-        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
-
-        #Creating text string to display( Color name and RGB values )
+    display_img = img.copy()
+    # Draw rectangle and text if a color has been selected
+    if xpos != 0 or ypos != 0:
+        cv2.rectangle(display_img,(20,20), (750,60), (b,g,r), -1)
         text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
-        
-        #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
-        cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
-
-        #For very light colours we will display text in black colour
+        cv2.putText(display_img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
         if(r+g+b>=600):
-            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
-            
-        clicked=False
-
+            cv2.putText(display_img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
+        # Draw a larger black circle with a thinner outline at the clicked position
+        cv2.circle(display_img, (xpos, ypos), 8, (0, 0, 0), 1)
+    cv2.imshow("image", display_img)
     #Break the loop when user hits 'esc' key    
     if cv2.waitKey(20) & 0xFF ==27:
         break
-    
 cv2.destroyAllWindows()
